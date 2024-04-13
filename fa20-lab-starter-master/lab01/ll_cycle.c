3a82bb629ac6851beb43391ba5b96d1b8273d6bd
xxbaizero0 <xxbaizero@gmail.com>
2024-04-13 19:53:08 +0800

    done lab01

--- fa20-lab-starter-master/lab01/ll_cycle.c
+++ fa20-lab-starter-master/lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    }
+    node* tortoise = head;
+    node* hare = head -> next;
+    while (hare != NULL && hare ->next != NULL) {
+        if (tortoise == hare) {
+            return 1;
+        }
+        hare = hare ->next ->next;
+        tortoise = tortoise ->next;
+    }
     return 0;
 }
