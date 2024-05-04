e12d09ca35146f33ca6fafcc44c4ea9fcab18e49
xxbaizero <xxbaizero@gmail.com>
2024-05-05 01:43:47 +0800

    done Lab3
    done Lab3

--- fa20-lab-starter-master/lab03/ex2.s
+++ fa20-lab-starter-master/lab03/ex2.s
@@ -1,6 +1,7 @@
 .globl main
 
 .data
+# 初始化数组
 source:
     .word   3
     .word   1
@@ -31,33 +32,37 @@ fun:
 main:
     # BEGIN PROLOGUE
     addi sp, sp, -20
-    sw s0, 0(sp)
-    sw s1, 4(sp)
-    sw s2, 8(sp)
+    sw s0, 0(sp)    # SUM
+    sw s1, 4(sp)    # SOURCE指针
+    sw s2, 8(sp)    # DEST指针
     sw s3, 12(sp)
     sw ra, 16(sp)
     # END PROLOGUE
-    addi t0, x0, 0
+	addi t0, x0, 0	# K
     addi s0, x0, 0
     la s1, source
     la s2, dest
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
+    slli s3, t0, 2   # 偏移量
+    add t1, s1, s3   # source偏移后的地址
+    lw t2, 0(t1)     # source[k]的值
+    # CONDITION
     beq t2, x0, exit
-    add a0, x0, t2
+    add a0, x0, t2   # a0存储source[k]的值，以便后面传参
+    # JUMP FUN
     addi sp, sp, -8
     sw t0, 0(sp)
     sw t2, 4(sp)
-    jal fun
+    jal fun          # a0存储着fun返回的值
     lw t0, 0(sp)
     lw t2, 4(sp)
     addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
+    # BACK
+    add t2, x0, a0   # t2代a0
+    add t3, s2, s3   # 偏移后的地址
+    sw t2, 0(t3)     
     add s0, s0, t2
+
     addi t0, t0, 1
     jal x0, loop
 exit:
--- fa20-lab-starter-master/lab03/factorial.s
+++ fa20-lab-starter-master/lab03/factorial.s
@@ -22,3 +22,18 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi sp, sp, -8
+    sw t1, 0(sp)
+    sw t2, 4(sp)
+    addi t1, x0, 1
+    add t2, a0, x0
+Loop:
+    beq t2, t1, Done
+    sub t2, t2, t1
+    mul a0, a0, t2
+    j Loop
+Done:
+    lw t2, 4(sp)
+    lw t1, 0(sp)
+    addi sp, sp, 8
+    jr ra
